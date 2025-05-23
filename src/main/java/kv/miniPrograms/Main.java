14cbe7188bd4b2df8a159139aa3a108290d9965d
kizzandev <69643444+kizzandev@users.noreply.github.com>
2025-05-23 20:02:52 -0300

    update Main

--- src/main/java/kv/miniPrograms/Main.java
+++ src/main/java/kv/miniPrograms/Main.java
@@ -1,8 +1,10 @@
 package kv.miniPrograms;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 import static kv.miniPrograms.PrimeNumbers.isPrime;
+import static kv.miniPrograms.Palindromes.isPalindrome;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,18 +16,41 @@ public class Main {
             System.out.println("Select the mini program to run.");
             System.out.println("0. Exit");
             System.out.println("1. isPrime");
+            System.out.println("2. isPalindrome");
 
             i = read.nextInt();
             switch (i) {
                 case 1:
                     main.runPrimeNumbers();
                     break;
+                case 2:
+                    main.runPalindromes();
                 default:
                     break;
             }
 
         } while (i != 0);
 
+    }
+
+    private void runPalindromes() {
+        Scanner read = new Scanner(System.in);
+        String phrase;
+
+        do {
+            System.out.print("Write a phrase (or nothing to exit): ");
+            phrase = read.nextLine();
+            if (Objects.equals(phrase, "")) {
+                break;
+            }
+
+            if (isPalindrome(phrase)) {
+                System.out.println(phrase + " is a palindrome.");
+            } else {
+                System.out.println(phrase + " is NOT a palindrome.");
+            }
+        }
+        while (true);
     }
 
     private void runPrimeNumbers() {
