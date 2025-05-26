c0edf7e91a7dff835573dea0c73008c90683a97c
kizzandev <69643444+kizzandev@users.noreply.github.com>
2025-05-26 18:51:36 -0300

    update Main

--- src/main/java/kv/miniPrograms/Main.java
+++ src/main/java/kv/miniPrograms/Main.java
@@ -9,8 +9,9 @@ import static kv.miniPrograms.Palindromes.isPalindrome;
 import static kv.miniPrograms.PasswordGenerator.generatePassword;
 
 public class Main {
+    private static final Scanner read = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
         int i;
         Main main = new Main();
 
@@ -21,19 +22,30 @@ public class Main {
                     1. Check if number is prime
                     2. Check if a phrase is a palindrome
                     3. Generate a password
-                    4. Factorial""");
+                    4. Factorial
+                    5. Stack
+                    6. Anagram""");
 
-            i = read.nextInt();
+            i = Integer.parseInt(read.nextLine());
             switch (i) {
                 case 1:
                     main.runPrimeNumbers();
                     break;
                 case 2:
                     main.runPalindromes();
+                    break;
                 case 3:
                     main.runPasswordGenerator();
+                    break;
                 case 4:
                     main.runFactorial();
+                    break;
+                case 5:
+                    main.runStack();
+                    break;
+                case 6:
+                    main.runAnagram();
+                    break;
                 default:
                     break;
             }
@@ -41,22 +53,68 @@ public class Main {
             System.out.println();
         } while (i != 0);
 
+        read.close();
+    }
+
+    private void runAnagram() {
+//        read.nextLine();
+        System.out.println("Anagram");
+        System.out.print("First string: ");
+        String first = read.nextLine();
+        System.out.print("Second string: ");
+        String second = read.nextLine();
+
+        if (Anagram.isAnagram(first, second)) {
+            System.out.println(first + " is an anagram of " + second);
+        } else {
+            System.out.println(first + " is NOT an anagram of " + second);
+        }
+    }
+
+    private void runStack() {
+        Stack<Integer> iStack = new Stack<>();
+        int opt;
+        System.out.println("Stack: " + iStack.toString());
+        do {
+            System.out.println("""
+                    Choose:
+                    0. exit
+                    1. add number
+                    2. remove number
+                    3. clear stack
+                    4. size""");
+            opt = Integer.parseInt(read.nextLine());
+            if (opt == 1) {
+                System.out.print("To add: ");
+                opt = Integer.parseInt(read.nextLine());
+                iStack.push(opt);
+                System.out.println("Stack: " + iStack.toString() + '\n');
+            } else if (opt == 2) {
+                iStack.pop();
+                System.out.println("Stack: " + iStack.toString() + '\n');
+            } else if (opt == 3) {
+                iStack.clear();
+                System.out.println("Stack: " + iStack.toString() + '\n');
+            } else if (opt == 4) {
+                System.out.println("Size: " + iStack.size() + '\n');
+            }
+        } while (opt != 0);
     }
 
     private void runFactorial() {
-        Scanner read = new Scanner(System.in);
         System.out.println("""
                 Choose:
                 1. normal int
                 2. big int""");
-        int n = read.nextInt();
+        int n = Integer.parseInt(read.nextLine());;
         System.out.print("Calculate the factorial of: ");
 
         if (n == 1) {
-            n = read.nextInt();
+            n = Integer.parseInt(read.nextLine());;
             System.out.println("Result: " + factorial(n));
         } else if (n == 2) {
-            BigInteger bn = read.nextBigInteger();
+//            BigInteger bn = read.nextBigInteger();
+            BigInteger bn = BigInteger.valueOf(Integer.parseInt(read.nextLine()));
             System.out.println("Result: " + factorial(bn));
         }
     }
@@ -67,7 +125,6 @@ public class Main {
     }
 
     private void runPalindromes() {
-        Scanner read = new Scanner(System.in);
         String phrase;
 
         do {
@@ -87,12 +144,11 @@ public class Main {
     }
 
     private void runPrimeNumbers() {
-        Scanner read = new Scanner(System.in);
         int n;
 
         do {
             System.out.print("Select a number: ");
-            n = read.nextInt();
+            n = Integer.parseInt(read.nextLine());;
             if (n == 0) {
                 break;
             }
